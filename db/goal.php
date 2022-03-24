8835ed5ad7d83fb441121ee70412565f6092f5c6
Gaurav Khati <gaurav@gmail.com>
2022-03-24 13:38:38 +0545

    hi

--- db/goal.php
+++ db/goal.php
@@ -3,10 +3,14 @@ include('connect.php');
 if (isset ($_POST['title'])){
     $goal =$_POST['title'];
     $descriptions =$_POST['descriptions'];
+    $accomplish_date =$_POST['accomplish_date'];
+    $status =$_POST['status'];
+     session_start();
+    $user_id = $_SESSION['user_id'];
 
         if($goal!=''){
-             $today = date('Y-m-d');    
-            $query = "INSERT INTO goal(title,descriptions) VALUES('$goal','$descriptions')";
+             
+            $query = "INSERT INTO goal(title,accomplish_date,descriptions,status,user_id) VALUES('$goal','$accomplish_date','$descriptions','$status','$user_id')";
                  if(mysqli_query($conn,$query)){
             $msg ="Successfully Inserted";
                 }else{
--- edit-goal.php
+++ edit-goal.php
@@ -51,12 +51,32 @@ else{
                         <input type="text" value="<?php echo $row['title'];?>" class="form-control" name="title">
                     </div>
                     <br/>
+
+                    <div class="date" style="float:left;">
+                       <label for="" >Accomplish Date</label>
+                       <input type="datetime-local" name="goal_accomplish_date" value="<?php echo $row['accomplish_date'];?>" class="form-control">
+                  </div>   <br><br>   <br>  <br>
+
                       <label>Description</label>
                     <div class="input-group">
                     <input type="text"  class="form-control"  value="<?php echo $row['descriptions'];?>" name="descriptions"> </input>
                           
                   </div>
                     <br/>
+                    <br/>
+                     <label>Status</label>
+                     <br>
+                     <div class="set_status"style="float:left;">
+                    <select class="form-select" name="goal_status"  value="<?php echo $row['status'];?>" aria-label="Default select example">
+                      <option selected>Complete</option>
+                      <option value="Complete">Complete</option>
+                      <option value="Incomplete">Incomplete</option>
+                      <option value="Running">Running</option>
+                    </select>
+                    </div>
+                  
+                  
+                  <input type="hidden" name="id" value="<?php echo $id;?>" ?>
                     <button type="submit" class="btn btn-dark">Save</button>
                 </form>
                <?php include('include/message.php'); ?>
@@ -91,11 +111,12 @@ else{
     </div>
   </div>
   
+  
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/82f2c2ba8a.js" crossorigin="anonymous"></script>
-<script src="js/bootbox.min.js"></script>
+
 
   </body>
 </html>
